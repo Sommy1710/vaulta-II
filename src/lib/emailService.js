4c0e4a3af0ec6aa9750948d32b65b6c8f5c5b30a
Sommy1710 <martinsomto2001@gmail.com>
2026-08-28 07:27:06 -0700

    email fix

--- src/lib/emailService.js
+++ src/lib/emailService.js
@@ -2,7 +2,7 @@ import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const transporter = nodemailer.createTransport({
+/*const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
         user: process.env.EMAIL_USER,
@@ -22,4 +22,41 @@ const sendEmail = async ({to, subject, html}) => {
     await transporter.sendMail(mailOptions);
 };
 
-export {transporter, sendEmail};
+export {transporter, sendEmail};*/
+
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    },
+});
+
+transporter.verify((error, success) => {
+    if (error) {
+        console.error("SMTP CONNECTION FAILED:", error);
+    } else {
+        console.log("SMTP SERVER IS READY");
+    }
+});
+
+const sendEmail = async ({ to, subject, html }) => {
+    const mailOptions = {
+        from: process.env.EMAIL_USER,
+        to,
+        subject,
+        html,
+    };
+
+    const info = await transporter.sendMail(mailOptions);
+
+    console.log("MESSAGE ID:", info.messageId);
+    console.log("ACCEPTED:", info.accepted);
+    console.log("REJECTED:", info.rejected);
+    console.log("RESPONSE:", info.response);
+
+    return info;
+};
+
+export { transporter, sendEmail };
